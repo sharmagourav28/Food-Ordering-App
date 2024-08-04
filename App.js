7c6dd6b3f0b36b7f3fd2d8e1aa7f105fd65b9303
Gourav Sharma <gouravsharma2806@gmail.com>
2024-08-04 23:09:51 +0530

    Props learning from siwggy api

--- App.js
+++ App.js
@@ -52,10 +52,11 @@ const HeaderComponents = () => {
 };
 
 const styleCard = {
-  backgroundColor: "yellowgreen",
+  backgroundColor: "#c8e3e2",
 };
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+  const { resData } = props;
   return (
     <div className="res-card" style={styleCard}>
       <img
@@ -63,7 +64,7 @@ const RestaurantCard = () => {
         src="https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         alt="res-img"
       />
-      <h3>Meghana Foods</h3>
+      <h3>{resData.name}</h3>
       <h4>Cakes,Cheez Cakes</h4>
       <h4>4.3 Start Rating</h4>
       <h4>38 Mins</h4>
@@ -71,12 +72,80 @@ const RestaurantCard = () => {
   );
 };
 
+const resObj = {
+  info: {
+    id: "67500",
+    name: "Pishori Restaurant",
+    cloudinaryImageId: "ntfzr5mgmt9b9f545vtl",
+    locality: "Sapna Sangeeta Road",
+    areaName: "Bhawar Kuan",
+    costForTwo: "₹450 for two",
+    cuisines: ["North Indian", "Mughlai", "Kebabs", "Biryani", "Chinese"],
+    avgRating: 4.3,
+    parentId: "158125",
+    avgRatingString: "4.3",
+    totalRatingsString: "10K+",
+    sla: {
+      deliveryTime: 26,
+      lastMileTravel: 4.9,
+      serviceability: "SERVICEABLE",
+      slaString: "25-30 mins",
+      lastMileTravelString: "4.9 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2024-08-04 23:59:00",
+      opened: true,
+    },
+    badges: {},
+    isOpen: true,
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        textBased: {},
+        imageBased: {},
+        textExtendedBadges: {},
+      },
+    },
+    aggregatedDiscountInfoV3: {
+      header: "ITEMS",
+      subHeader: "AT ₹109",
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {},
+      },
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: {},
+    externalRatings: {
+      aggregatedRating: {
+        rating: "4.0",
+        ratingCount: "1K+",
+      },
+      source: "GOOGLE",
+      sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+    },
+    ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+  },
+  analytics: {},
+  cta: {
+    link: "swiggy://menu?restaurant_id=67500",
+    text: "RESTAURANT_MENU",
+    type: "DEEPLINK",
+  },
+};
+
 const BodyComponents = () => {
   return (
     <div className="body">
       <div className="search">search</div>
       <div className="rest-container">
-        <RestaurantCard />
+        <RestaurantCard resData={resObj} />
       </div>
     </div>
   );
