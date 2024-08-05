c646a35d6534cdb05f1837d446ac7c8af278731c
Gourav Sharma <gouravsharma2806@gmail.com>
2024-08-05 11:04:35 +0530

    Lecture 4 Done

--- App.js
+++ App.js
@@ -57,95 +57,482 @@ const styleCard = {
 
 const RestaurantCard = (props) => {
   const { resData } = props;
+  const { name, cuisines, avgRating, cloudinaryImageId } = resData?.data;
   return (
     <div className="res-card" style={styleCard}>
       <img
         className="res-logo"
-        src="https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
         alt="res-img"
       />
-      <h3>{resData.info.name}</h3>
-      <h4>{resData.info.cuisines}</h4>
-      <h4>{resData.info.avgRating}</h4>
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating}</h4>
       <h4>38 Mins</h4>
     </div>
   );
 };
 
-const resObj = {
-  info: {
-    id: "67500",
-    name: "Pishori Restaurant",
-    cloudinaryImageId: "ntfzr5mgmt9b9f545vtl",
-    locality: "Sapna Sangeeta Road",
-    areaName: "Bhawar Kuan",
-    costForTwo: "₹450 for two",
-    cuisines: ["North Indian", "Mughlai", "Kebabs", "Biryani", "Chinese"],
-    avgRating: 4.3,
-    parentId: "158125",
-    avgRatingString: "4.3",
-    totalRatingsString: "10K+",
-    sla: {
-      deliveryTime: 26,
-      lastMileTravel: 4.9,
-      serviceability: "SERVICEABLE",
-      slaString: "25-30 mins",
-      lastMileTravelString: "4.9 km",
-      iconType: "ICON_TYPE_EMPTY",
-    },
-    availability: {
-      nextCloseTime: "2024-08-04 23:59:00",
-      opened: true,
-    },
-    badges: {},
-    isOpen: true,
-    type: "F",
-    badgesV2: {
-      entityBadges: {
-        textBased: {},
-        imageBased: {},
-        textExtendedBadges: {},
+const resList = [
+  {
+    info: {
+      id: "667764",
+      name: "Burger Farm",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/5/10/213a964e-8671-4d10-aed6-4d5f0b4e2441_667764.JPG",
+      locality: "Veda Complex",
+      areaName: "Bhawar Kuan",
+      costForTwo: "₹250 for two",
+      cuisines: ["Burgers", "Beverages", "Snacks", "Desserts"],
+      avgRating: 4.4,
+      parentId: "4660",
+      avgRatingString: "4.4",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 28,
+        lastMileTravel: 5.6,
+        serviceability: "SERVICEABLE",
+        slaString: "25-30 mins",
+        lastMileTravelString: "5.6 km",
+        iconType: "ICON_TYPE_EMPTY",
       },
-    },
-    aggregatedDiscountInfoV3: {
-      header: "ITEMS",
-      subHeader: "AT ₹109",
-    },
-    differentiatedUi: {
-      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-      differentiatedUiMediaDetails: {
-        mediaType: "ADS_MEDIA_ENUM_IMAGE",
-        lottie: {},
-        video: {},
+      availability: {
+        nextCloseTime: "2024-08-05 04:00:00",
+        opened: true,
       },
-    },
-    reviewsSummary: {},
-    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-    restaurantOfferPresentationInfo: {},
-    externalRatings: {
-      aggregatedRating: {
-        rating: "4.0",
-        ratingCount: "1K+",
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          textBased: {},
+          imageBased: {},
+          textExtendedBadges: {},
+        },
       },
-      source: "GOOGLE",
-      sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+      aggregatedDiscountInfoV3: {
+        header: "40% OFF",
+        subHeader: "UPTO ₹80",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
     },
-    ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    analytics: {},
+    cta: {
+      link: "swiggy://menu?restaurant_id=667764",
+      text: "RESTAURANT_MENU",
+      type: "DEEPLINK",
+    },
   },
-  analytics: {},
-  cta: {
-    link: "swiggy://menu?restaurant_id=67500",
-    text: "RESTAURANT_MENU",
-    type: "DEEPLINK",
+  {
+    info: {
+      id: "359782",
+      name: "Grameen Kulfi",
+      cloudinaryImageId: "wsm4rrcc2ed2ves9kvoi",
+      locality: "SHRIKRISNA CHAMBER",
+      areaName: "DHENU MARKET",
+      costForTwo: "₹120 for two",
+      cuisines: ["Ice Cream", "Desserts"],
+      avgRating: 4.7,
+      veg: true,
+      parentId: "12175",
+      avgRatingString: "4.7",
+      totalRatingsString: "100+",
+      sla: {
+        deliveryTime: 24,
+        lastMileTravel: 5,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "5.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-08-05 00:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          textExtendedBadges: {},
+          textBased: {},
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                  description: "pureveg",
+                },
+              },
+            ],
+          },
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹99",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {},
+    cta: {
+      link: "swiggy://menu?restaurant_id=359782",
+      text: "RESTAURANT_MENU",
+      type: "DEEPLINK",
+    },
   },
-};
+  {
+    info: {
+      id: "67500",
+      name: "Pishori Restaurant",
+      cloudinaryImageId: "ntfzr5mgmt9b9f545vtl",
+      locality: "Sapna Sangeeta Road",
+      areaName: "Bhawar Kuan",
+      costForTwo: "₹450 for two",
+      cuisines: ["North Indian", "Mughlai", "Kebabs", "Biryani", "Chinese"],
+      avgRating: 4.3,
+      parentId: "158125",
+      avgRatingString: "4.3",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 26,
+        lastMileTravel: 4.9,
+        serviceability: "SERVICEABLE",
+        slaString: "25-30 mins",
+        lastMileTravelString: "4.9 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-08-04 23:59:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          textBased: {},
+          imageBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹109",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "4.0",
+          ratingCount: "1K+",
+        },
+        source: "GOOGLE",
+        sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {},
+    cta: {
+      link: "swiggy://menu?restaurant_id=67500",
+      text: "RESTAURANT_MENU",
+      type: "DEEPLINK",
+    },
+  },
+  {
+    info: {
+      id: "67386",
+      name: "Shree Leela Kitchen",
+      cloudinaryImageId: "a1jcoxj83vijycgew0w9",
+      locality: "Chhoti Gwaltoli",
+      areaName: "Chhoti Gwaltoli",
+      costForTwo: "₹300 for two",
+      cuisines: ["North Indian", "Indian"],
+      avgRating: 4.3,
+      parentId: "185050",
+      avgRatingString: "4.3",
+      totalRatingsString: "1K+",
+      promoted: true,
+      adTrackingId:
+        "cid=16319601~p=1~adgrpid=16319601#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=67386~eid=90fa863a-be44-4c77-872f-9c75891e7f70~srvts=1722792130013~collid=45995",
+      sla: {
+        deliveryTime: 27,
+        lastMileTravel: 4.4,
+        serviceability: "SERVICEABLE",
+        slaString: "25-30 mins",
+        lastMileTravelString: "4.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-08-04 23:59:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          textBased: {},
+          imageBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹50 OFF",
+        subHeader: "ABOVE ₹199",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "3.8",
+          ratingCount: "500+",
+        },
+        source: "GOOGLE",
+        sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {},
+    cta: {
+      link: "swiggy://menu?restaurant_id=67386",
+      text: "RESTAURANT_MENU",
+      type: "DEEPLINK",
+    },
+  },
+  {
+    info: {
+      id: "70504",
+      name: "Ramesh South Indian",
+      cloudinaryImageId: "ynd31yd3mds6vpxgl2kd",
+      locality: "Jawaharmargh",
+      areaName: "Sarafa",
+      costForTwo: "₹150 for two",
+      cuisines: ["South Indian", "Indian"],
+      avgRating: 4.5,
+      veg: true,
+      parentId: "166607",
+      avgRatingString: "4.5",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 20,
+        lastMileTravel: 1.8,
+        serviceability: "SERVICEABLE",
+        slaString: "15-20 mins",
+        lastMileTravelString: "1.8 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-08-04 23:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Rxawards/_CATEGORY-South%20Indian.png",
+            description: "Delivery!",
+          },
+          {
+            imageId: "v1695133679/badges/Pure_Veg111.png",
+            description: "pureveg",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          textBased: {},
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  imageId: "Rxawards/_CATEGORY-South%20Indian.png",
+                  description: "Delivery!",
+                },
+              },
+              {
+                attributes: {
+                  imageId: "v1695133679/badges/Pure_Veg111.png",
+                  description: "pureveg",
+                },
+              },
+            ],
+          },
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "20% OFF",
+        subHeader: "UPTO ₹50",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {},
+    cta: {
+      link: "swiggy://menu?restaurant_id=70504",
+      text: "RESTAURANT_MENU",
+      type: "DEEPLINK",
+    },
+  },
+  {
+    info: {
+      id: "96055",
+      name: "Red Chilli Restaurant",
+      cloudinaryImageId: "hdffrrsfznb0kdwwyqr7",
+      locality: "Rajmohalla",
+      areaName: "Mukeri Pura",
+      costForTwo: "₹300 for two",
+      cuisines: ["Indian", "North Indian"],
+      avgRating: 4.4,
+      parentId: "168288",
+      avgRatingString: "4.4",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 16,
+        lastMileTravel: 1.5,
+        serviceability: "SERVICEABLE",
+        slaString: "15-20 mins",
+        lastMileTravelString: "1.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-08-04 23:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textExtendedBadges: {},
+          textBased: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "20% OFF",
+        subHeader: "UPTO ₹45",
+        discountTag: "POCKET HERO",
+        discountCalloutInfo: {
+          message: "Free Delivery",
+          logoCtx: {
+            logo: "v1655895371/free_delivery_logo_hqipbo.png",
+          },
+        },
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {},
+    cta: {
+      link: "swiggy://menu?restaurant_id=96055",
+      text: "RESTAURANT_MENU",
+      type: "DEEPLINK",
+    },
+  },
+];
 
 const BodyComponents = () => {
   return (
     <div className="body">
       <div className="search">search</div>
       <div className="rest-container">
-        <RestaurantCard resData={resObj} />
+        {resList.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
       </div>
     </div>
   );
