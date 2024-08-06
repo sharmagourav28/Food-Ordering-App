399acf8015ff97157eb19f1866f996b5758ad76f
Gourav Sharma <gouravsharma2806@gmail.com>
2024-08-06 14:22:04 +0530

    know hoe to fect data from api but error occur

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,20 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+
+import { useState, useEffect } from "react";
 const Body = () => {
-  const [listofRestaurant, setListofRestautant] = useState(resList);
+  const [listofRestaurant, setListofRestautant] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    setListofRestautant(json?.data.cards[2]?.data?.data?.cards);
+  };
   return (
     <div className="body">
       <div className="filter">
@@ -21,7 +33,7 @@ const Body = () => {
       </div>
       <div className="rest-container">
         {listofRestaurant.map((restaurant) => (
-          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
     </div>
