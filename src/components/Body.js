e0dcdb6b1f0ac19046551f05acc2215de398efb4
Gourav Sharma <gouravsharma2806@gmail.com>
2024-08-12 14:36:09 +0530

    dynamic routing added in cards features

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,8 @@
 import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
+import { Link } from "react-router-dom";
+
 const Body = () => {
   const [listofRestaurant, setListofRestautant] = useState([]);
   const [filterRestaurant, setFilterRestaurant] = useState([]);
@@ -76,7 +78,12 @@ const Body = () => {
       </div>
       <div className="rest-container">
         {filterRestaurant.map((restaurant) => (
-          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+          <Link
+            key={restaurant.info.id}
+            to={"restaurants/" + restaurant.info.id}
+          >
+            <RestaurantCard resData={restaurant} />
+          </Link>
         ))}
       </div>
     </div>
